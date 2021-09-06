7aa8f548fa2d07886c50875041eef293e9600d09
wsameer <dev.wsameer@gmail.com>
2021-09-06 20:01:02 +0530

    feat(fonts): adds custom font using tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,34 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
     purge: [
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}'
     ],
-    darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        fontFamily: {
+            sans: ['Quicksand', ...defaultTheme.fontFamily.sans]
+        },
+        extend: {
+            gridColumnEnd: {
+                n1: '-1'
+            },
+            gridRowEnd: {
+                n1: '-1'
+            }
+        },
+        boxShadow: {
+            '7xl': '0 0 6px 3px rgb(30 30 31 / 59%)',
+            '8xl': '0 3px 6px rgb(0 0 0 / 16%), 0 3px 6px rgb(0 0 0 / 23%)',
+            none: 'none'
+        }
     },
-    variants: {
-        extend: {},
-    },
+    darkMode: 'media',
     plugins: [],
+    variants: {
+        extend: {
+            fontSmoothing: ['hover', 'focus'],
+            boxShadow: ['responsive', 'hover', 'focus', 'active']
+        }
+    }
 };
