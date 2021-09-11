f8898224924537a43fa8a834283c7c51df365b88
wsameer <dev.wsameer@gmail.com>
2021-09-11 10:11:00 +0530

    feat: adds lang attribute to html

--- next.config.js
+++ next.config.js
@@ -4,5 +4,9 @@ module.exports = {
     reactStrictMode: true,
     images: {
         domains: ['medly.com', 'www.securly.com', 'www.google.com']
+    },
+    i18n: {
+        locales: ['en'],
+        defaultLocale: 'en'
     }
 };
