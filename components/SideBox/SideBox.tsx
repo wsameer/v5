42bf29385fa8797a9fe7231ddd5fac8c1718aed4
wsameer <dev.wsameer@gmail.com>
2022-05-02 11:14:40 -0400

    feat: adds location component in the sideBox

--- components/SideBox/SideBox.tsx
+++ components/SideBox/SideBox.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import JobTitle from './JobTitle';
 import PersonName from './PersonName';
 import { IMAGE_SRC } from './constant';
+import { MapPin } from 'react-feather';
 
 const Divider: React.FC = () => (
     <div className="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
@@ -16,9 +17,15 @@ export const SideBox: React.FC = () => {
                 <Avatar width={'180'} srcFile={IMAGE_SRC} bgColor="purple" />
                 <div className="mt-4 flex flex-col items-center text-center justify-center">
                     <PersonName name={'Sameer Waskar'} />
-                    <JobTitle value={'Full Stack Developer'} />
+                    <JobTitle value={'Engineering Manager'} />
                     <Divider />
                     <Socials />
+                </div>
+            </div>
+            <div className="pb-4 mt-4 md:p-4 md:mt-4 mb-4 md:px-4 w-full md:w-72 lg:w-80 bg-gray-800 shadow-lg text-center">
+                <div className="inline-flex">
+                    <MapPin className="mt-2 text-gray-300" />
+                    <p className="mt-2 mx-2 text-gray-300">Toronto, Canada</p>
                 </div>
             </div>
             <div className="pb-8 md:p-10 md:mt-4 mb-4 md:px-4 w-full md:w-72 lg:w-80 bg-gray-800 shadow-lg text-center">
